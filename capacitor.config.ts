2a3267bbd39e85c7024bb855b1366e706b9a5f43
Ezequiel Julian Hurt <ezequiel-julian.hurt@basf.com>
2024-07-22 17:19:59 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'my-scale-garmin-connector',
+  webDir: 'dist'
+};
+
+export default config;

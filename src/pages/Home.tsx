29abe5100925a2734bb5c1a5d7292ac3e5f0f708
Ezequiel Julian Hurt <ezequiel-julian.hurt@basf.com>
2024-07-24 17:23:02 +0200

    installed bluetooth plugin

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,37 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { BleClient } from '@capacitor-community/bluetooth-le';
+import { useEffect } from 'react';
+
+
+
 
 const Home: React.FC = () => {
+
+  const initializeBle = async () => {
+    try {
+      await BleClient.initialize();
+    }
+    catch (error) {
+      console.error('Error initializing BLE', error);
+    }
+  };
+
+  useEffect(() => {
+    initializeBle();
+  }, []);
+
+  const startScan = async () => {
+    try {
+      await BleClient.requestLEScan({}, (device) => {
+        console.log('Device found', device);
+      });
+    }
+    catch (error) {
+      console.error('Error starting scan', error);
+    }
+  };
   return (
     <IonPage>
       <IonHeader>
@@ -17,6 +46,7 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
+        <IonButton onClick={startScan}>Start Scan</IonButton>
       </IonContent>
     </IonPage>
   );
